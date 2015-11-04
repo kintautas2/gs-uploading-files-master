81c30b3d19567afad3d9d912ceff56e4a58fdaa4
kintautas2 <kintautas@ventila.lt>
2015-11-04 10:11:10 +0200

    uzduotis
    uzduotis

---
+++ src/main/java/Veikimas/Algortimas.java
@@ -0,0 +1,27 @@
+package Veikimas;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.*;
+import java.util.*;
+
+@Controller
+public class Algortimas {
+   
+    
+    
+    
+}
